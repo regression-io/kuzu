1162cd59878ee504176b9ea31c99a99302a9a8d2
Chang Liu <liuc223@gmail.com>
2023-11-13 18:30:31 +0800

    X

--- tools/wasm/main.cpp
+++ tools/wasm/main.cpp
@@ -14,15 +14,23 @@ int main() {
         auto database = std::make_unique<Database>("tmp", systemConfig);
         std::cout << "Database created." << std::endl;
         auto connection = std::make_unique<Connection>(database.get());
+        std::cout << "Connection created." << std::endl;
+
+        // Run a CALL
+        auto callResult = connection->query("CALL db_version() RETURN *;");
+        std::cout << callResult->toString();
 
         // Create schema.
         connection->query("CREATE NODE TABLE Person(name STRING, age INT64, PRIMARY KEY(name));");
+        std::cout << "Schema created." << std::endl;
         // Create nodes.
         connection->query("CREATE (:Person {name: 'Alice', age: 25});");
         connection->query("CREATE (:Person {name: 'Bob', age: 30});");
+        std::cout << "Nodes created." << std::endl;
 
         // Execute a simple query.
         auto result = connection->query("MATCH (a:Person) RETURN a.name AS NAME, a.age AS AGE;");
+        std::cout << "Query executed." << std::endl;
         // Print query result.
         std::cout << result->toString();
     } catch (const std::exception& e) {
